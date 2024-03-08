f264af9b79c0e4ef7952f588bc3b00c09a2f371e
felipe <dev.felipemignone@gmail.com>
2024-03-07 23:18:34 -0300

    add bootstrap with npm

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const expressEjsLayout = require('express-ejs-layouts');
+const express = require("express");
+const expressEjsLayout = require("express-ejs-layouts");
 let homeRoute = require("./routes/homeRoute");
 let usuarioRoute = require("./routes/usuarioRoute");
 let loginRoute = require("./routes/loginRoute");
@@ -12,20 +12,18 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 app.set("layout", "./layout");
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use("/bootstrap", express.static("./node_modules/bootstrap/dist/"));
 app.use(expressEjsLayout);
 
-
 //configura as rotas existentes no nosso sistema
-app.use("/",  homeRoute);
+app.use("/", homeRoute);
 app.use("/usuarios", usuarioRoute);
 app.use("/login", loginRoute);
 
-
-
 //inicia o nosso servidor web
-app.listen(5000, function() {
-    console.log("servidor web iniciado")
-})
+app.listen(5000, function () {
+  console.log("servidor web iniciado");
+});
