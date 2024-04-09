7d3184cf3d69d11e033de6b168ef73084a5c58d8
felipe <dev.felipemignone@gmail.com>
2024-04-08 21:10:13 -0300

    add debbuger launch file

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -4,7 +4,5 @@ import HomeController from '../controllers/homeController.js';
 const router = Router();
 let ctrl = new HomeController();
 router.get("/", ctrl.homeView);
-router.get("/contato", ctrl.contatoView);
-router.get('/semlayout', ctrl.semLayoutView);
 
 export default router;
