a8ad5d67d3cd52ef48b4196d5a80543bc3bc1ffd
Felipe <dev.felipemignone@gmail.com>
2024-04-10 20:06:34 -0300

    create products controller

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -6,6 +6,10 @@ class ProdutosController {
     res.render("./produtos/listagem");
   }
 
+  editarView(req, res) {
+    res.render("./produtos/editar");
+  }
+
   //Methods
   async getTodosProdutos(req, res) {
     let prodModel = new ProdutoModel();
@@ -22,6 +26,81 @@ class ProdutosController {
     res.send({
       ok: false,
       data: null,
+    });
+  }
+
+  async getProdutoPorId(req, res) {
+    const id = req.params.id;
+    let prodModel = new ProdutoModel(id, null, null, null);
+
+    let produto = await prodModel.getProdutoPorId();
+
+    if (produto) {
+      res.send({
+        ok: true,
+        data: produto,
+      });
+      return;
+    }
+
+    res.send({
+      ok: false,
+      data: null,
+    });
+  }
+
+  async addNovoProduto(req, res) {
+    const { nome, valor, origemId } = req.body;
+    let prodModel = new ProdutoModel(null, nome, valor, origemId);
+    let isCreated = await prodModel.addNovoProduto();
+
+    if (isCreated) {
+      res.send({
+        ok: true,
+        message: "Produto adicionado com sucesso!",
+      });
+      return;
+    }
+    res.send({
+      ok: false,
+      message: "Não foi possível adicionar poduto",
+    });
+  }
+
+  async editarProduto(req, res) {
+    const { nome, valor, origemId } = req.body;
+    const id = req.params.id;
+    let prodModel = new ProdutoModel(id, nome, valor, origemId);
+    let isUpdated = await prodModel.updateProdutoPorId();
+
+    if (isUpdated) {
+      res.send({
+        ok: true,
+        message: "Produto alterado com sucesso!",
+      });
+      return;
+    }
+    res.send({
+      ok: false,
+      message: "Não foi possível alterar poduto",
+    });
+  }
+
+  async deleteProduto(req, res) {
+    const id = req.params.id;
+    let prodModel = new ProdutoModel(id, null, null, null);
+    let isDeleted = await prodModel.deleteProdutoPorId();
+
+    if (isDeleted) {
+      res.send({
+        ok: true,
+        message: "Produto excluido com sucesso!",
+      });
+      return;
+    }
+    res.send({
+      ok: false,
+      message: "Não foi possível excluir poduto",
     });
   }
 }
